afaab289b676244a75cb5586a553b6fe40b3458e
SJY <bangstation@naver.com>
2025-03-26 15:28:18 +0900

    first ERD

---
+++ src/main/java/com/ultimate/umc_study1/UmcStudy1Application.java
@@ -0,0 +1,13 @@
+package com.ultimate.umc_study1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcStudy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcStudy1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ultimate/umc_study1/UmcStudy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultimate.umc_study1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcStudy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
